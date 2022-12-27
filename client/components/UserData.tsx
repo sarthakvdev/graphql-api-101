099572f2583838439bbd80de8bd69ddf5f4f5e2e
Sarthak Verma <55709030+sarthakvdev@users.noreply.github.com>
2022-12-27 23:22:20 +0530

    added movie and moviebyname query in client

--- client/components/UserData.tsx
+++ client/components/UserData.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from "react";
-import { gql, useQuery } from "@apollo/client";
+import React, { FC, useEffect, useState } from "react";
+import { gql, useQuery, useLazyQuery } from "@apollo/client";
 
 const QUERY_ALL_USERS = gql`
   query GetAllUsers {
@@ -12,8 +12,41 @@ const QUERY_ALL_USERS = gql`
   }
 `;
 
+const QUERY_ALL_MOVIES = gql`
+  query GetAllMovies {
+    movies {
+      id
+      name
+      yearOfRelease
+      isInTheaters
+    }
+  }
+`;
+
+const GET_MOVIE_BY_NAME = gql`
+  query getMovieByName($name: String!) {
+    movie(name: $name) {
+      name
+      yearOfRelease
+    }
+  }
+`;
+
 const UserData: FC = () => {
   const { data, loading } = useQuery(QUERY_ALL_USERS);
+  const { data: movieData } = useQuery(QUERY_ALL_MOVIES);
+  const [fetchMovie, { data: movieSearchedData, error: movieSearchedError }] =
+    useLazyQuery(GET_MOVIE_BY_NAME);
+
+  const [movieSearched, setMovieSearched] = useState("");
+
+  useEffect(() => {
+    console.log("movies", movieData);
+  }, [movieData]);
+
+  useEffect(() => {
+    console.log("moviessearched", movieSearchedData);
+  }, [movieSearchedData]);
 
   return (
     <>
@@ -28,6 +61,43 @@ const UserData: FC = () => {
               <p>Age: {user.age}</p>
             </div>
           ))}
+        </div>
+      )}
+      {movieData && (
+        <div className="flex flex-col gap-y-4 mt-5">
+          <p className="text-5xl">Movies list</p>
+          {movieData.movies.map((movie: any) => (
+            <div key={movie.id} className="text-left p-3 border rounded-lg">
+              <p>Name: {movie.name}</p>
+              <p>Release Year: {movie.yearOfRelease}</p>
+            </div>
+          ))}
+        </div>
+      )}
+      <div className="mt-5 flex gap-x-3">
+        <input
+          type="text"
+          className="ring-1 ring-gray-200 active:ring-blue-100 border-none rounded-lg px-4"
+          placeholder="The Dark Knight..."
+          onChange={(event) => setMovieSearched(event.target.value)}
+        />
+        <button
+          className="rounded-lg px-4 py-2 bg-blue-300 active:bg-blue-400 text-white font-medium"
+          onClick={() => {
+            fetchMovie({
+              variables: {
+                name: movieSearched,
+              },
+            });
+          }}
+        >
+          Search Movie
+        </button>
+      </div>
+      {movieSearchedData && (
+        <div className="text-left py-3 px-6 border rounded-lg mt-3 ">
+          <p>Name: {movieSearchedData.movie.name}</p>
+          <p>Release Year: {movieSearchedData.movie.yearOfRelease}</p>
         </div>
       )}
     </>
