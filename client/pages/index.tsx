44aa1c534dec88b620db06a55e6c0830c3bff27d
Sarthak Verma <55709030+sarthakvdev@users.noreply.github.com>
2022-12-27 15:13:16 +0530

    created client(nextjs) & server dirs

---
+++ client/pages/index.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <Head>
+        <title>GraphQL 101</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+
+      </main>
+
+      <footer className="flex h-24 w-full items-center justify-center border-t">
+        <p>
+          Built by
+          <a
+            href="https://sarthak.wtf"
+            className="underline ml-1"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Sarthak
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
